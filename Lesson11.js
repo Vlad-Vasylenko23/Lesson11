1a70c058d4f9765510ee06f7d84f64ab0c57062e
Vlad-Vasylenko23 <144223479+Vlad-Vasylenko23@users.noreply.github.com>
2024-02-12 23:21:14 +0200

    Add files via upload

---
+++ Lesson11.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const readline = require('readline')
+  
+function countWords(st) { 
+  return st.trim().split(/\s+/).length
+} 
+ 
+const Stream = fs.createReadStream("Lection5.txt")
+const rl = readline.createInterface({ 
+  input: Stream, 
+  output: process.stdout, 
+  terminal: false 
+})
+ 
+let total = 0; 
+ 
+rl.on('line', (line) => { 
+  console.log(line)
+  const wordCount = countWords(line)
+  total += wordCount
+})
+ 
+rl.on('close', () => { 
+  console.log(`Total word count: ${total}`)
+  process.exit(0)
+})
